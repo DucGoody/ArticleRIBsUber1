b84f618acef8e89f19c8909a60882ea19cf47625
HoangVanDuc <hoangvanduc101095@gmail.com>
2019-12-23 11:30:06 +0700

    Initial Commit

---
+++ ArticleRIBsUber/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ArticleRIBsUber
+//
+//  Created by HoangVanDuc on 12/23/19.
+//  Copyright © 2019 HoangVanDuc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
